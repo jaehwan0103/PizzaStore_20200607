d44203a63b79c875d9da55824030757a4cab3cda
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-07 14:39:22 +0900

    data format 수정

--- app/src/main/java/kr/ptus/pizzastore_20200607/data/PizzaStore.kt
+++ app/src/main/java/kr/ptus/pizzastore_20200607/data/PizzaStore.kt
@@ -3,7 +3,7 @@ package kr.ptus.pizzastore_20200607.data
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 
-class PizzaStore (val name : String)  {
+class PizzaStore (val name : String, val logoUrl : String, phoneNum : String)  {
 
 
 
